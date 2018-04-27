a9679e48ae8efaad454c23f791c303de5e94c793
Xu Xiaochun <xuxiaochun27@163.com>
2018-04-27 22:26:13 +0800

    Update RootLayoutController.java

--- PillarTrackerGUI/src/main/java/com/nus/mbi/pillar/tracker/gui/RootLayoutController.java
+++ PillarTrackerGUI/src/main/java/com/nus/mbi/pillar/tracker/gui/RootLayoutController.java
@@ -4277,7 +4277,7 @@ public class RootLayoutController implements Initializable {
             }     
        });       
        link_mbi.setVisible(true);       
-       link_copyright.setText("Source Code on Github");
+       link_copyright.setText("Source Code on Github under GPL3.0 License");
        link_copyright.setOnAction((ActionEvent e)->{
             if (Desktop.isDesktopSupported()) {
                     try {
